92f7d484de641792efa5164e1553604d4e436189
Habibur Rahman <43277090+hrhridoy@users.noreply.github.com>
2022-04-24 00:32:59 +0600

    added pages folder and books page

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import 'virtual:windi.css';
+const book = document.querySelector('#book');
+
+// let data = fetch('https://gutendex.com/books')
+//   .then((res) => res.body)
+//   .then((res) => console.log(res))
+//   .catch(console.error());
+// console.log(data);
