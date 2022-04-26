4f08f67fe4683c5af9a6bea21456a20faa878c4d
Habibur Rahman <43277090+hrhridoy@users.noreply.github.com>
2022-04-26 22:55:52 +0600

    fixed issues related to fetching data ( lol just learned to fetch and display the data recieved from the API. Need to learn more about this.)

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 import 'virtual:windi.css';
 const book = document.querySelector('#book');
 
-// let data = fetch('https://gutendex.com/books')
-//   .then((res) => res.body)
-//   .then((res) => console.log(res))
-//   .catch(console.error());
-// console.log(data);
+function fetchData() {
+  fetch('https://gutendex.com/books')
+    .then((res) => res.json())
+    .then((data) => {
+      if (typeof data === 'object') {
+        for (const datas of data.results) {
+          // console.log(typeof datas.formats['image/jpeg']);
+          let imageUrl = datas.formats['image/jpeg'];
+          book.innerHTML += `
+         <h3>${datas.title}</h3>
+         <img src=${imageUrl} />
+         `;
+        }
+      } else if (typeof data === 'string') {
+        return JSON.parse(data);
+      } else if (!data) {
+        return console.log('No responce from the API server');
+      }
+    })
+    .catch((err) => console.log(err));
+}
+window.addEventListener('onload', fetchData());
